bf25cda7df00ea831a04d0cf18f889dca9b1fcfb
RAMIVAR <RAMIVAR11@HOTMAIL.COM>
2021-06-30 21:24:27 -0500

    Commit Inicial

---
+++ fibonacci.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class fibonacci {
+
+    public static void main (String [] args){
+
+        Scanner Leer = new Scanner (System.in);
+        int Limite; 
+        int primero = 0, segundo = 1, media;
+
+System.out.print("ingresa limite: ");
+Limite =Leer.nextInt();
+
+
+for (int i=0; primero<=Limite; i++){
+  
+    media=primero;
+    primero=segundo;
+    segundo = (media + segundo);
+
+System.out.println(media);
+//System.out.println(primero);
+//System.out.println(segundo);
+
+
+}
+
+}
+
+
+}
